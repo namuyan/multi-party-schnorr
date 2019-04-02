bdee24e814d1cd3f4dc1f60df7a1a68a27b95279
namuyan <thhjuu@yahoo.co.jp>
2019-04-02 23:29:07 +0900

    trivial change fix python script

--- aggregate_1_of_1.py
+++ aggregate_1_of_1.py
@@ -2,12 +2,13 @@ import multi_party_schnorr
 
 
 msg = b"nice meet"
-eph = multi_party_schnorr.PyEphemeralKey()  # 5uS
+eph = multi_party_schnorr.PyEphemeralKey()  # 18mS
 print("key:", eph.keypair.get_public_key().hex())
-R, sig = eph.get_single_sign(msg)  # 18ms
+R, sig = eph.get_single_sign(msg)  # 19ms
 print("R:", R.hex())
 print("sig:", sig.hex())
 result = multi_party_schnorr.verify_aggregate_sign(sig, R, eph.keypair.get_public_key(), msg)  # 35ms
 print("res:", result)
 # from timeit import timeit
 # timeit("single()", globals=globals(), number=10)
+# check = lambda x: timeit(x, globals=globals(), number=10)*1000//10
--- aggregate_n_of_n.py
+++ aggregate_n_of_n.py
@@ -1,29 +1,30 @@
 import multi_party_schnorr
+from timeit import timeit
 
-pair0 = multi_party_schnorr.PyKeyPair()
+pair0 = multi_party_schnorr.PyKeyPair()  # 27mS
 pair1 = multi_party_schnorr.PyKeyPair()
 pair2 = multi_party_schnorr.PyKeyPair()
 print("create 3 pairs", pair0, pair1, pair2)
 signers = [x.get_public_key() for x in (pair0, pair1, pair2)]
-eph0 = multi_party_schnorr.PyEphemeralKey()
+eph0 = multi_party_schnorr.PyEphemeralKey()  # 19.5mS
 eph1 = multi_party_schnorr.PyEphemeralKey()
 eph2 = multi_party_schnorr.PyEphemeralKey()
 print("create 3 ephemeral", eph0, eph1, eph2)
-ephemeris = [x.keypair.get_public_key() for x in (eph0, eph1, eph2)]
-agg0 = multi_party_schnorr.PyAggregate.generate(signers, ephemeris, pair0, eph0)
-agg1 = multi_party_schnorr.PyAggregate.generate(signers, ephemeris, pair1, eph1)
-agg2 = multi_party_schnorr.PyAggregate.generate(signers, ephemeris, pair2, eph2)
+ephemeral = [x.keypair.get_public_key() for x in (eph0, eph1, eph2)]
+agg0 = multi_party_schnorr.PyAggregate.generate(signers, ephemeral, pair0, eph0)  # 53mS
+agg1 = multi_party_schnorr.PyAggregate.generate(signers, ephemeral, pair1, eph1)
+agg2 = multi_party_schnorr.PyAggregate.generate(signers, ephemeral, pair2, eph2)
 print("create 3 aggregates {} {} {}".format(agg0, agg1, agg2))
 msg = b"hello world"
-sig0 = agg0.get_partial_sign(msg)
+sig0 = agg0.get_partial_sign(msg)  # 0.021mS
 sig1 = agg1.get_partial_sign(msg)
 sig2 = agg2.get_partial_sign(msg)
-sig01 = agg0.add_signature_parts(sig0, sig1)
+sig01 = agg0.add_signature_parts(sig0, sig1)  # 0.011mS
 sig012 = agg0.add_signature_parts(sig01, sig2)
-R = agg0.R()
-apk = agg0.apk()
+R = agg0.R()  # 0.0025mS
+apk = agg0.apk()  # 0.0011mS
 print("sig", sig012.hex())
 print("apk:", apk.hex())
 print("R:", R.hex())
-res = multi_party_schnorr.verify_aggregate_sign(sig012, R, apk, msg)
+res = multi_party_schnorr.verify_aggregate_sign(sig012, R, apk, msg)  # 35.5mS
 print("result:", res)
